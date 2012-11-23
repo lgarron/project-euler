596e17f02ca285108fe414d08d1b02ed77b3dccf
Lucas Garron <lucasg@gmx.de>
2012-11-23 01:12:22 -0800

    1.hs: Parametric limit.

--- 1.hs
+++ 1.hs
@@ -1,8 +1,11 @@
 import Data.List
 
+pLimit :: Integer
+pLimit = 1000-1 -- *less than* 1000
+
 multiples :: [Integer]
 --multiples = [x | x<-[1..999], mod x 3 == 0 || mod x 5 == 0]
-multiples = union [3, 6..999] [5, 10..999]
+multiples = union [3, 6..pLimit] [5, 10..pLimit]
 
 result :: Integer
 result = sum multiples
