cedb4b605bf4b3b5c66bb3ccbe1dbcdda45d1e27
Lucas Garron <lucasg@gmx.de>
2012-11-23 16:25:04 -0800

    4.hs

---
+++ 4.hs
@@ -0,0 +1,20 @@
+import Data.Digits
+import Data.List
+
+num3 :: [Integer]
+num3 = [100..999]
+
+products :: [Integer]
+products = [x*y | x<-num3, y<-num3]
+
+palindromic :: Eq a => [a] -> Bool
+palindromic list = (reverse list == list)
+
+palindromes :: [Integer]
+palindromes = filter (palindromic . (digits 10)) products
+
+result :: Integer
+result = last $ sort palindromes
+
+main :: IO ()
+main = do print result
