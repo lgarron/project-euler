96aa41d6bb8a7563749e2a19c575d48687d4918c
Lucas Garron <lucasg@gmx.de>
2012-12-01 17:51:52 -0800

    15-manual-memo.hs

---
+++ 15-manual-memo.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+binomial :: (Integer -> Integer -> Integer) -> Integer -> Integer -> Integer
+binomial bRec x 0 = 1
+binomial bRec 0 y = 0
+binomial bRec x y = (bRec (x-1) (y-1)) + (bRec (x-1) y)
+
+idx2 :: [[a]] -> Integer -> Integer -> a
+idx2 list i j = (list `genericIndex` i) `genericIndex` j
+
+binomialMemo :: [[Integer]]
+binomialMemo = [[binomial (idx2 binomialMemo) x y | y <- [0..]] | x <- [0..]]
+
+result :: Integer
+result = idx2 binomialMemo 40 20
+
+main :: IO ()
+main = do print result
