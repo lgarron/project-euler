88b3b38a6cb41792a446f2a13c7d42a5d00753a6
Lucas Garron <lucasg@gmx.de>
2012-12-20 22:14:46 -0800

    18.hs

---
+++ 18.hs
@@ -0,0 +1,22 @@
+-- Project Euler, Problem #18
+-- Lucas Garron
+-- Date: December 20, 2012
+
+import Data.Digits -- cabal install digits
+import qualified Data.MemoCombinators as Memo -- cabal install data-memocombinators
+
+-- Code
+
+parseInput :: String -> [[Integer]]
+parseInput = map (map read . words) . lines
+
+nextLine :: [Integer] -> [Integer] -> [Integer]
+nextLine l1 = zipWith (+) $ zipWith max (l1 ++ [0]) (0:l1)
+
+result :: String -> Integer
+result str = maximum $ foldl1 nextLine $ parseInput str
+
+-- Output
+
+main :: IO ()
+main = do readFile "18_in.txt" >>= print . result
