595b3247048c2c651a18619c360dc7c65b0f9b5d
Lucas Garron <lucasg@gmx.de>
2012-12-21 19:19:22 -0800

    19.hs

---
+++ 19.hs
@@ -0,0 +1,23 @@
+-- Project Euler, Problem #19
+-- Lucas Garron
+-- Date: December 21, 2012
+
+import Data.List -- genericLength
+import Data.Time
+import Data.Time.Calendar.WeekDate
+
+-- Code
+
+-- Help from http://rosettacode.org/wiki/Day_of_the_week#Haskell
+isSunday :: Day -> Bool
+isSunday day = wday == 7
+    where (_,_,wday) = toWeekDate day
+ 
+result :: Integer
+result = genericLength $ filter isSunday days where
+  days = [fromGregorian year month 1 | year <- [1901..2000], month <- [1..12]]
+
+-- Output
+
+main :: IO ()
+main = print result
