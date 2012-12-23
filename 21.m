48a33fd082e3077e9a3b20d306a9e584868131cb
Lucas Garron <lucasg@gmx.de>
2012-12-23 11:52:33 -0800

    21.m

---
+++ 21.m
@@ -0,0 +1,9 @@
+(* Project Euler, Problem #21 *)
+(* Lucas Garron *)
+(* Date: December 23, 2012 *)
+
+d[n_] := DivisorSigma[1, n] - n;
+AmicableQ[n_] := (d[n] != n && d[d[n]] == n);
+result = Total@Select[Range[10000], AmicableQ];
+
+Print[result];
