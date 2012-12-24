244d694b58e8d169771c6d1abae9607bcd9b0fbb
Lucas Garron <lucasg@gmx.de>
2012-12-23 16:15:10 -0800

    23.m

---
+++ 23.m
@@ -0,0 +1,13 @@
+(* Project Euler, Problem #23 *)
+(* Lucas Garron *)
+(* Date: December 23, 2012 *)
+
+AbundantQ[n_Integer] := DivisorSigma[1, n] > 2 n;
+
+(* Not efficient, but it works. *)
+abundantNumbers = Select[Range[28123], AbundantQ];
+NotAbundantSum[n_Integer] :=  Intersection[abundantNumbers, n - abundantNumbers] == {};
+
+result = Total@Select[Range[28123], NotAbundantSum];
+
+Print[result];
