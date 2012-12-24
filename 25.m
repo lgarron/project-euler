c176b0d829baa246cdb9e09938a0159877aa4024
Lucas Garron <lucasg@gmx.de>
2012-12-23 18:04:07 -0800

    25.m

---
+++ 25.m
@@ -0,0 +1,8 @@
+(* Project Euler, Problem #25 *)
+(* Lucas Garron *)
+(* Date: December 23, 2012 *)
+
+(* Go Math! *)
+result = result = Log[GoldenRatio, (10^(1000-1))*Sqrt[5]] // Ceiling
+
+Print[result];
