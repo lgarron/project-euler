d3320d7b66d15ca7b1d350172937fc5138bda071
Lucas Garron <lucasg@gmx.de>
2012-12-26 16:09:39 -0800

    27.m

---
+++ 27.m
@@ -0,0 +1,26 @@
+(* Project Euler, Problem #27 *)
+(* Lucas Garron *)
+(* Date: December 26, 2012 *)
+
+PrimeRun[a_Integer, b_Integer] := Module[{n = 0},
+  While[
+    PrimeQ[n^2 + a n + b],
+    n++
+  ];
+  Return[n];
+]
+
+max = 0;
+maxPair = {};
+Do[
+  If[
+    PrimeRun[a, b] > max,
+    max = PrimeRun[a, b]; maxPair = {a, b}
+  ],
+  {a, -999, 999},
+  {b, -999, 999}
+]
+
+result = Times @@ maxPair
+
+Print[result];
