d2bc01edb56126ad2f9e16a0ed2b425a7e974148
Lucas Garron <lucasg@gmx.de>
2012-12-26 16:16:09 -0800

    27.c

---
+++ 27.c
@@ -0,0 +1,57 @@
+/* Project Euler, Problem #27
+ * Lucas Garron
+ * Date: December 26, 2012
+ */
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "inc/sieve.h"
+
+#define MAX 999
+#define SIEVE_SIZE 1000000
+
+bool* primeQ; // Will be malloced.
+
+bool isPrime(int n) {
+  if (n<0) {
+    return false;
+  }
+  assert(n < SIEVE_SIZE);
+  return primeQ[n];
+}
+
+int primeRun(int a, int b) {
+  int n = 0;
+  while (isPrime(n*n + a*n+b)) {
+    n++;
+  }
+  return n;
+}
+
+int main() {
+
+  primeQ = sieve(SIEVE_SIZE);
+
+  int best = 0;
+  int bestCoefficientProduct = 0;
+
+  int a, b;
+  for (a = -MAX; a <= MAX; a++) {
+    for (b = -MAX; b <= MAX; b++) {
+      int run = primeRun(a, b);
+      if (run > best) {
+        best = run;
+        bestCoefficientProduct = a * b;
+      }
+    }
+  }
+
+  printf("%d\n", bestCoefficientProduct);
+
+  free(primeQ);
+
+  return 0;
+}
