702af15434a17bd8cc4e97554709401514b244ae
Lucas Garron <lucasg@gmx.de>
2012-12-26 16:30:01 -0800

    28.hs

---
+++ 28.hs
@@ -0,0 +1,13 @@
+-- Project Euler, Problem #28
+-- Lucas Garron
+-- Date: December 26, 2012
+
+-- Code
+
+result :: Integer
+result = 1 + 4 * sum [4 * x^2 + x + 1 | x <-[1..500]]
+
+-- Output
+
+main :: IO ()
+main = do print result
