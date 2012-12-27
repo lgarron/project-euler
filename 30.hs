21d038c05c5d9b3b8f19421e6996faa1382125c1
Lucas Garron <lucasg@gmx.de>
2012-12-26 16:38:46 -0800

    30.hs

---
+++ 30.hs
@@ -0,0 +1,18 @@
+-- Project Euler, Problem #30
+-- Lucas Garron
+-- Date: December 26, 2012
+
+import Data.Digits (digits) -- cabal install digits
+
+-- Code
+
+isDigitPowerSum :: Integer -> Integer -> Bool
+isDigitPowerSum k n = (sum $ map (^ k) $ digits 10 n) == n
+
+--result :: Integer
+result = sum [n | n<-[2..10^6], isDigitPowerSum 5 n]
+
+-- Output
+
+main :: IO ()
+main = do print result
