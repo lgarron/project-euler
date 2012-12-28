7c1f753813de57392a34c8194ecaa3c8da894ea8
Lucas Garron <lucasg@gmx.de>
2012-12-27 17:50:31 -0800

    37.hs

---
+++ 37.hs
@@ -0,0 +1,21 @@
+-- Project Euler, Problem #37
+-- Lucas Garron
+-- Date: December 27, 2012
+
+import Data.Digits (digits, unDigits) -- cabal install digits
+import Data.List (inits, tails)
+import Data.Numbers.Primes (isPrime)
+
+-- Code
+
+isTruncatablePrime :: Integer -> Bool
+isTruncatablePrime n = from (tail . inits) && from (init . tails) where
+  from headsOrTails = and $ map (isPrime . unDigits 10) $ headsOrTails $ digits 10 n
+
+result :: Integer
+result = sum $ take 11 $ filter isTruncatablePrime [10..]
+
+-- Output
+
+main :: IO ()
+main = print result
