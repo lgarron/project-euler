5c3317bbc54b2072ae7c2cc5d2ccf5e13b11edad
Lucas Garron <lucasg@gmx.de>
2012-12-28 02:01:59 -0800

    41.hs

---
+++ 41.hs
@@ -0,0 +1,20 @@
+-- Project Euler, Problem #41
+-- Lucas Garron
+-- Date: December 28, 2012
+
+import Data.Digits (unDigits) -- cabal install digits
+import Data.List (permutations)
+import Data.Numbers.Primes (isPrime)
+
+-- Code
+
+pandigitalPrimes :: Integer -> [Integer]
+pandigitalPrimes numDigits = filter isPrime $ map (unDigits 10) $ permutations [1..numDigits]
+
+result :: Integer
+result = maximum . concat $ map pandigitalPrimes [1..9]
+
+-- Output
+
+main :: IO ()
+main = print result
