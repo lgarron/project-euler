dcb425703cc0239174e1d56dcb77e826d68ca1ab
Lucas Garron <lucasg@gmx.de>
2012-12-30 06:38:53 -0800

    6.hs -- Cleanup.

--- 6.hs
+++ 6.hs
@@ -1,5 +1,15 @@
+-- Project Euler, Problem #6
+-- Lucas Garron
+-- Date: November 23, 2012
+
+-- Code
+
 result :: Integer
-result = (sum [1..100])^2 - (sum $ map (^2) [1..100])
+result = squaredSum - sumOfSquares where
+  squaredSum = (sum [1..100])^2
+  sumOfSquares = sum $ map (^2) [1..100]
+
+-- Output
 
 main :: IO ()
-main = do print result
+main = print result
