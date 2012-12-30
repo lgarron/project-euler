230ea5c72f56bf7feceda8c8cd036881a029b1a1
Lucas Garron <lucasg@gmx.de>
2012-12-30 06:32:40 -0800

    3.m -- Cleanup.

--- 3.m
+++ 3.m
@@ -1,1 +1,11 @@
-Print[FactorInteger[600851475143][[-1, 1]]]
+(* Project Euler, Problem #3 *)
+(* Lucas Garron *)
+(* Date: November 23, 2012 *)
+
+(* Code *)
+
+result = FactorInteger[600851475143][[-1, 1]];
+
+(* Output *)
+
+Print[result];
