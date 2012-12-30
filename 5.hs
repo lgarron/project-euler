8acf2245b12c355447a80a9e870055bce85c88f3
Lucas Garron <lucasg@gmx.de>
2012-12-30 06:35:16 -0800

    5.hs -- Cleanup.

--- 5.hs
+++ 5.hs
@@ -1,5 +1,13 @@
+-- Project Euler, Problem #5
+-- Lucas Garron
+-- Date: November 23, 2012
+
+-- Code
+
 result :: Integer
 result = foldl1 lcm [1..20]
 
+-- Output
+
 main :: IO ()
-main = do print result
+main = print result
