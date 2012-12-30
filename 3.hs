e76ee9dab5a37c6fb3cfc087a32c3ef6c2c47839
Lucas Garron <lucasg@gmx.de>
2012-12-30 06:32:24 -0800

    3.hs -- Cleanup.

--- 3.hs
+++ 3.hs
@@ -1,7 +1,15 @@
-import Data.Numbers.Primes -- cabal install primes
+-- Project Euler, Problem #3
+-- Lucas Garron
+-- Date: November 23, 2012
+
+import Data.Numbers.Primes (primeFactors) -- cabal install primes
+
+-- Code
 
 result :: Integer
 result = last $ primeFactors 600851475143
 
+-- Output
+
 main :: IO ()
-main = do print result
+main = print result
