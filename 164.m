44a34221e7bac6b03ce6044cce5e85c0dc673f49
Lucas Garron <code@garron.net>
2014-09-05 03:05:28 -0700

    164.m: Marginally slower but significantly neater code.

--- 164.m
+++ 164.m
@@ -4,26 +4,11 @@
 
 (* Code *)
 
-Do[
-  Num[2, i, j] = Boole[i > 0 && i + j <= 9],
-  {i, 0, 9},
-  {j, 0, 9}
-];
+Num[2, i_, j_] := Boole[i > 0 && i + j <= 9];
 
-Do[
-  Do[
-    Num[index, i, j] = 0,
-    {i, 0, 9},
-    {j, 0, 9}
-  ];
-
-  Do[
-    Num[index, last, current] += Num[index - 1, penultimate, last],
-    {penultimate, 0, 9},
-    {last, 0, 9},
-    {current, 0, 9 - last - penultimate}
-  ],
-  {index, 3, 20}
+Num[index_, last_, current_] := Num[index, last, current] = Sum[
+  Num[index - 1, penultimate, last],
+  {penultimate, 0, 9 - last - current}
 ];
 
 result = Sum[
