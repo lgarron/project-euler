17753f94c95b509dfff37a7251caf0ce8ebfb98e
Lucas Garron <code@garron.net>
2014-09-09 03:22:59 -0700

    106.m: Add comments.

--- 106.m
+++ 106.m
@@ -5,11 +5,11 @@
 (* Code *)
 
 result = Length[Select[
-  Tuples[Table[{0, 1, -1}, {12}]],
-  Count[#, 1] == Count[#, -1] &&
-  MemberQ[Accumulate[#], 1] &&
-  MemberQ[Accumulate[#], -1]&
-]]/2;
+  Tuples[Table[{0, 1, -1}, {12}]], (* Of all the ways of assigning the 12 digits to {no set, larger set, smaller set}, *)
+  Count[#, 1] == Count[#, -1] &&   (* take those with the same amound ot elements in each set, *)
+  MemberQ[Accumulate[#], 1] &&     (* where the larger set has more elements in some initial segment, and *)
+  MemberQ[Accumulate[#], -1]&      (* the smaller set also has more elements in some initial segment, and *)
+]]/2;                              (* divide for symmetry. *)
 
 (* Output *)
 
